b99a4e2a31ed820c47885d2a97b8ca105758cb4b
Clay Kaufmann <claykaufmann@gmail.com>
2022-05-09 12:18:23 -0400

    add kfold turb for vacc

---
+++ Deep_Learning/turb_kfold_vacc.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#SBATCH --partition=dggpu
+
+# Request Nodes
+#SBATCH --nodes=2
+
+# Req CPU Cores
+#SBATCH --ntasks=2
+
+# Req GPUs
+#SBATCH --gres=gpu:8
+
+# Req Memory
+#SBATCH --mem=30G
+
+# Run for x minutes
+#SBATCH --time=1000
+
+# Name of job
+#SBATCH --job-name=SleepersRiverTurbidityPeakDetection
+
+# Name output file
+#SBATCH --output=%x_j%j.out
+
+# Email
+#SBATCH --mail-type=ALL
+
+source ~/.bashrc
+
+cd ${SLURM_SUBMIT_DIR}
+
+# Executable section: echoing some Slurm data
+echo "Running host:    ${SLURMD_NODENAME}"
+echo "Assigned nodes:  ${SLURM_JOB_NODELIST}"
+echo "Job ID:          ${SLURM_JOBID}"
+conda activate srrw
+time python turb_kfold.py
